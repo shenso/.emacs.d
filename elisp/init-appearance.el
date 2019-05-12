dc9296c447002dc034d649ffbb4170028abf3408
Shawn Henson <shawnhenson@knights.ucf.edu>
2019-05-12 00:40:38 -0400

    created init script, got rid of gui elements (like toolbar)

---
+++ elisp/init-appearance.el
@@ -0,0 +1,7 @@
+(provide 'init-appearance)
+
+;; universal appearance settings
+;;; disable toolbar/gui stuff
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
---
+++ init.el
@@ -0,0 +1,4 @@
+;; load startup scripts
+(add-to-list 'load-path "~/.emacs.d/elisp")
+;;; initialize packaging before configuration
+(require 'init-appearance)

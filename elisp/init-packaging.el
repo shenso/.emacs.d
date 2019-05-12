89e1b6ee16149a1e9e3dc836b309c47286af4425
Shawn Henson <shawnhenson@knights.ucf.edu>
2019-05-12 00:42:50 -0400

    added melpa, configured and added use-package

---
+++ elisp/init-packaging.el
@@ -0,0 +1,20 @@
+(provide 'init-packaging)
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
+;; install use-package if needed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; make use-package install packages
+(require 'use-package)
+(setq use-package-always-ensure t)
--- init.el
+++ init.el
@@ -1,4 +1,8 @@
+;; move custom data out of my init script dammit!
+(setq custom-file "~/.emacs.d/custom.el")
+
 ;; load startup scripts
 (add-to-list 'load-path "~/.emacs.d/elisp")
 ;;; initialize packaging before configuration
+(require 'init-packaging)
 (require 'init-appearance)

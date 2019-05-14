60a5fd2d18917008352d4e527a43de3991ccea57
Shawn Henson <shawnhenson@knights.ucf.edu>
2019-05-14 19:08:04 -0400

    setup org-agenda

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -1,3 +1,11 @@
+(require 'org)
+(setq notes-path "~/documents/notes/")
+
+;; org-agenda
+(setq org-agenda-files (list notes-path
+			     (concat notes-path "agenda")))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
 ;; org-crypt
 (require 'org-crypt)
 (setq org-crypt-key "shawnhenson@knights.ucf.edu")
@@ -5,7 +13,7 @@
 ;; org-journal
 (use-package org-journal
   :config
-  (setq org-journal-dir "~/documents/notes/journal"
+  (setq org-journal-dir (concat notes-path "journal")
 	org-journal-file-type 'weekly
 	org-journal-enable-encyption t
 	org-journal-encrypt-journal t))

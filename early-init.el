fb068992e7b3bc2da7510fd4204c676adfba165b
Shawn Henson <shenson@empara.io>
2025-05-27 16:26:16 -0400

    Increase GC and proc output thresholds

--- early-init.el
+++ early-init.el
@@ -1,5 +1,8 @@
 (setq package-enable-at-startup nil)
 
+(setq gc-cons-threshold 104857600) ; 100mb
+(setq read-process-output-max (* 1048576)) ; 1mb
+
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)

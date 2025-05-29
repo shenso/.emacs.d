07d393eb5f07f894240cf5f196b1e8c03c522b0f
Shawn Henson <shenson@empara.io>
2025-05-29 18:23:31 -0400

    Add dape

--- init.el
+++ init.el
@@ -398,6 +398,33 @@
     (add-hook 'theme-timer-change-hook (lambda ()
                                          (acm-frame-init-colors t)))))
 
+(use-package dape
+  :straight t
+  :init
+  (setq dape-prefix-key "\C-x\C-a")
+  :config
+  ;; Info buffers to the right
+  (setq dape-buffer-window-arrangement 'right)
+
+  ;; Info buffers like gud (gdb-mi)
+  (setq dape-buffer-window-arrangement 'gud)
+  (setq dape-info-hide-mode-line nil)
+
+  ;; Pulse source line (performance hit)
+  (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+
+  ;; Showing inlay hints
+  (setq dape-inlay-hints t)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+
+  ;; Kill compile buffer on build success
+  (add-hook 'dape-compile-hook 'kill-buffer)
+
+  ;; Projectile users
+  (setq dape-cwd-function 'projectile-project-root))
+
 (use-package ement
   :straight t
   :commands ement-connect)
